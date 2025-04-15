587976bcddb4ab90d0fa5c0ecffea6663c317bc1
Md saiduzzaman saad <142034434+saiduzzamansaad@users.noreply.github.com>
2025-04-15 23:48:11 +0600

    Update main.jsx
    code update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
 
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-2YMST8S8SY"); // replace with your Measurement ID
+ReactGA.send("pageview");
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
